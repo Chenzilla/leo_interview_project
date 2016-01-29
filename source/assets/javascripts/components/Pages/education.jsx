17ffb3ce052591483f0c8269ee65999132b45f18
Alex Chen <alexander.chen@yale.edu>
2016-01-28 23:49:10 -0500

    Fun - add education page

--- source/assets/javascripts/components/Pages/education.jsx
+++ source/assets/javascripts/components/Pages/education.jsx
@@ -2,7 +2,12 @@ var Education = React.createClass({
   render: function() {
     return (
       <div className="overview-container">
-        I am Education Page
+        <div className="education__request">
+            Find out more about any diseases here:
+          </div>
+          <div className="education__button">
+            <button><a href="http://www.webmd.com">I want to learn</a></button>
+        </div>
       </div>
     )
   }
