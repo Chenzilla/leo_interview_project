68a5191c24bfed1095d9e8a3a6fd1e416f54f7d6
Alex Chen <alexander.chen@yale.edu>
2016-01-28 23:46:12 -0500

    Fun - add RX page

--- source/assets/javascripts/components/Pages/rx.jsx
+++ source/assets/javascripts/components/Pages/rx.jsx
@@ -2,7 +2,13 @@ var RX = React.createClass({
   render: function() {
     return (
       <div className="overview-container">
-        I am RX Page
+        <div className="rx__request">
+          Please place any additional orders here:
+        </div>
+        <div className="rx__button">
+          <button><a href="https://www.walgreens.com">ReFill my prescription</a></button>
+        </div>
+
       </div>
     )
   }
